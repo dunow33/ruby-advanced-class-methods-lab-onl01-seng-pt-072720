4cc68f29a2b9e552510bf546a965ab08e14ca612
Brian Donnoe <dunow33@hotmail.com>
2020-08-17 17:39:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,10 @@ class Song
     self.all.sort_by { | song | song.name }
   end
   
-  def self.new_from_filename
-    
+  def self.new_from_filename(song)
+    song_array = song.split("-")
+    artist_name = song_array[0]
+    name_array = song_array[1].split(".")
+    name = name_array[0]
   end
 end
