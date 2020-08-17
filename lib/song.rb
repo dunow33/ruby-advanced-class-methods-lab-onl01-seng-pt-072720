9556d930fc8b8b85b5cf9e6a9348e27269eb305e
Brian Donnoe <dunow33@hotmail.com>
2020-08-17 16:35:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,43 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.each do | song |
+      if song.name == name
+        return song
+      end
+    end
+    nil
+  end
+  
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    
+    if song == nil
+      song = self.create_by_name(name)
+    end
+    
+    song
+  end
+  
 end
